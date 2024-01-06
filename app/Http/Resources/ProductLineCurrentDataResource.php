c8f8376d5cf3268e13ebb1a29aba18f0f59eda64
khaled abodaif <khaled.abodaif@yahoo.com>
2024-01-06 03:59:26 +0200

    switch image to return array

--- app/Http/Resources/ProductLineCurrentDataResource.php
+++ app/Http/Resources/ProductLineCurrentDataResource.php
@@ -18,12 +18,30 @@ class ProductLineCurrentDataResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'description' => $this->description,
-            'image' => asset("assets/images/").'/'.$this->image,
+            'image' =>self::getImageResource($this->image),
             'pdf' => asset("assets/images/").'/'.$this->pdf,
             'youtube' => $this->youtube ?? '' ,
             'instagram' => $this->instagram ?? '',
             'facebook' => $this->facebook ?? '',
             'shop' => $this->shop ?? ''
              ];
+    }
+
+    public static function getImageResource($images){
+
+        $alpoum=[];
+
+        if (is_null(json_decode($images)))
+             $alpoum[]= ['image'=>$images];
+
+        else {
+            foreach (json_decode($images) as $key => $image) {
+                $alpoum[] =
+                    asset("assets/images/") . '/' . $image
+                ;
+            }
+        }
+        return $alpoum;
+
     }
 }
